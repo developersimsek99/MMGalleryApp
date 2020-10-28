c6b395c4ddbe79de755b47f5f4d7344d3995891a
MEHMET ARICI <arc424267@gmail.com>
2020-10-28 17:44:09 +0300

    deneme3

--- MSMA1/MSMA1/Form1.Designer.cs
+++ MSMA1/MSMA1/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.arabaDgw = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.arabaDgw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Uygulamadan Çık";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // arabaDgw
+            // 
+            this.arabaDgw.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.arabaDgw.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.arabaDgw.Location = new System.Drawing.Point(12, 40);
+            this.arabaDgw.Name = "arabaDgw";
+            this.arabaDgw.Size = new System.Drawing.Size(775, 187);
+            this.arabaDgw.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.arabaDgw);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.arabaDgw)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView arabaDgw;
     }
 }
 
--- MSMA1/MSMA1/Form1.cs
+++ MSMA1/MSMA1/Form1.cs
@@ -16,5 +16,16 @@ namespace MSMA1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ArabaDal arabaDal = new ArabaDal();
+            //arabaDgw.DataSource = arabaDal.Listele();
+        }
     }
 }
