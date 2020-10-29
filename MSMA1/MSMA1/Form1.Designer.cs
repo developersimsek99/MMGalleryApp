543dbf691f071d6f563f4ef0ea33371a7cfe94f1
Murat Şimşek <udemydeveloper99@gmail.com>
2020-10-29 14:18:00 +0300

    Silme butonu eklendi

--- MSMA1/MSMA1/Form1.Designer.cs
+++ MSMA1/MSMA1/Form1.Designer.cs
@@ -59,7 +59,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
+            this.button2.Text = "Sil";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
